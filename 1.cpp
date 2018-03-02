36d8e2265861b75e818b2557520a0ca853cb29d5
Rimil.mandrita <31147874+Rimilmandrita@users.noreply.github.com>
2018-03-02 09:32:27 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,8 @@
+
 #include <iostream>
 using namespace std;
 
-int binarySearch(int a[50],int beg, int last,int num)
+int binarySearch1(int a[50],int beg, int last,int num)
 {
 	
 	while(beg<=last)
@@ -20,25 +21,62 @@ int binarySearch(int a[50],int beg, int last,int num)
    	beg=mid+1;
    }
 	}
+	return -1;
+}
+
+int binarySearch2(int a[50],int beg, int last,int num)
+{
+	
+	while(beg<=last)
+	{
+	int mid=(beg+last)/2;
+	if(a[mid]==num)
+   {
+   	return mid+1;
+   }
+   else if(num<a[mid])
+   {
+   	beg=mid+1;
+   }
+   else 
+   {
+   	last=mid-1;
+   }
+	}
+	return -1;
 }
 
 int main() {
-int arr[100],n,num;
-cout<<"Enter the size of array."
+int arr[100],n,num,pos;
+cout<<"Enter the size of array:";
 cin>>n;
-cout<<"Enter the elements of array in sorted order(ascending or descending)."
-(for int i=0;i<n;i++)
+cout<<n;
+cout<<"\nEnter the elements of array in sorted order(ascending or descending).";
+cout<<"\nThe array is:";
+for (int i=0;i<n;i++)
 {
 	cin>>arr[i];
 	cout<<arr[i]<<" ";
 }
-cout<<"Enter the element to be searched."
+cout<<"\nEnter the element to be searched:";
 cin>>num;
+cout<<num;
 
 int beg=0;
 int last=n-1;
+{
+if(arr[0]<arr[1])
+{
+ pos=binarySearch1(arr,beg,last,num);
+}
+else
+pos=binarySearch2(arr,beg,last,num);
+}
 
-binarySearch(arr,beg,last)
+if(pos==-1)
+cout<<"\nThe element is not found.";
+else
+cout<<"\nElement found at "<<pos<<"  th position";
 
 	return 0;
-}
+}	
